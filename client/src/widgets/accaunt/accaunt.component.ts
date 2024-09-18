fe225bdebd26502384369bcdd1c41b955bfe8d4d
einsy <eins.yeager@gmail.com>
2024-09-18 12:29:27 +0500

    accaunt update

--- client/src/widgets/accaunt/accaunt.component.ts
+++ client/src/widgets/accaunt/accaunt.component.ts
@@ -1,8 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { LogoutComponent } from './assets/logout.component';
 import { RouterLink } from '@angular/router';
-import { LoginModalComponent } from '../../features/login.modal/login.modal.component';
+import { LoginModalComponent } from '@features/login.modal/login.modal.component';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { AppStore } from '@app/providers/store';
+import { selectProfile } from '@app/providers/store/profile';
+import { Role } from '../../entities';
+import { ProfileI } from '../../entities/profile/interface';
 
 @Component({
   selector: 'app-accaunt',
@@ -15,16 +20,20 @@ import { CommonModule } from '@angular/common';
           src="assets/avatar.jpg"
           alt="avatar"
           class="avatar w-full aspect-square rounded-full cursor-pointer hover:scale-110 transition duration-200"
-          routerLink="profile"
+          [routerLink]="profile?.role === Role.GUEST ? null : '/profile'"
         />
       </div>
-      <div class=" flex flex-col">
-        <div class="username">{{ username }}</div>
+      <div class=" flex flex-col align-center justify-center">
+        <div class="username">
+          {{ profile?.role === Role.GUEST ? '' : profile?.name }}
+        </div>
         <div
           class="logout_container flex cursor-pointer text-center gap-2 transition duration-200 hover:scale-105"
           (click)="handleClick()"
         >
-          <div class="logout_text">Выйти</div>
+          <div class="logout_text">
+            {{ profile?.role === Role.GUEST ? 'Войти' : 'Выйти' }}
+          </div>
           <app-logout />
         </div>
       </div>
@@ -43,13 +52,18 @@ import { CommonModule } from '@angular/common';
 })
 export class AccauntComponent {
   @Input() class = '';
-  username = 'Алексей Алексеевич Толиков';
+  profile: ProfileI | null = null;
   modal = false;
-  constructor() {
-    this.username =
-      this.username.split(' ')[0] + ' ' + this.username.split(' ')[1];
+  Role = Role;
+  constructor(private store: Store<AppStore>) {
+    this.store.select(selectProfile).subscribe((state) => {
+      this.profile = state;
+    });
+    if (!this.profile?.name) return;
+    this.profile.name = this.profile.name.split(' ').slice(0, 2).join(' ');
   }
   handleClick() {
     this.modal = true;
+    console.log(this.profile?.role);
   }
 }
